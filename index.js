eea97b17392f834738e0e1d0230cf967c569e051
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-05-18 16:43:08 -0300

    Adicionado o modo dark/light

--- index.js
+++ index.js
@@ -33,14 +33,44 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
   body.classList.toggle('light');
   body.classList.toggle('dark');
+  toggleGitHubIconColor();
+  toggleImageGradient();
+  toggleButtonText(); // Adicionado para alternar o texto do botão
 });
 
 // Set the initial theme based on user preference
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
   body.classList.add('dark');
+  toggleImageGradient(); // Aplica a cor correta do gradiente para o modo dark
+  toggleButtonText(); // Adicionado para definir o texto do botão na inicialização
 } else {
   body.classList.add('light');
 }
 
+function toggleGitHubIconColor() {
+  const githubIcon = document.querySelector('.bxl-github');
+  const bxllinkedin = document.querySelector('.bxl-linkedin');
+  const bxlinstagram = document.querySelector('.bxl-instagram');
+  if (body.classList.contains('dark')) {
+    githubIcon.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+    bxllinkedin.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+    bxlinstagram.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+  } else {
+    githubIcon.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
+    bxllinkedin.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
+    bxlinstagram.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
+  }
+}
 
-  
+function toggleImageGradient() {
+  const imgWrapper = document.querySelector('.img-wrapper');
+  imgWrapper.classList.toggle('light'); // Alternar a classe light na .img-wrapper
+}
+
+function toggleButtonText() {
+  if (body.classList.contains('dark')) {
+    toggleButton.textContent = 'Modo Light'; // Alterando o texto do botão para "Modo Light" no Modo Dark
+  } else {
+    toggleButton.textContent = 'Modo Dark'; // Alterando o texto do botão para "Modo Dark" no Modo Light
+  }
+}
