47f38b833e78f6fdbf0e67ec498a84bedb26978f
Angelo Belelli Bugni <130174985+Angelobelelli@users.noreply.github.com>
2024-05-20 12:51:00 -0300

    .

--- index.js
+++ index.js
@@ -27,50 +27,54 @@ overlay.addEventListener('click' , ()=> {
 
 
 
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+  const toggleButton = document.getElementById('theme-toggle');
+  const body = document.body;
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('light');
-  body.classList.toggle('dark');
-  toggleGitHubIconColor();
-  toggleImageGradient();
-  toggleButtonText(); // Adicionado para alternar o texto do botão
-});
+  toggleButton.addEventListener('click', () => {
+    body.classList.toggle('light');
+    body.classList.toggle('dark');
+    toggleGitHubIconColor();
+    toggleImageGradient();
+    toggleButtonText(); // Adicionado para alternar o texto do botão
+  });
 
-// Set the initial theme based on user preference
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  // Set the initial theme to dark
   body.classList.add('dark');
   toggleImageGradient(); // Aplica a cor correta do gradiente para o modo dark
   toggleButtonText(); // Adicionado para definir o texto do botão na inicialização
-} else {
-  body.classList.add('light');
-}
 
-function toggleGitHubIconColor() {
-  const githubIcon = document.querySelector('.bxl-github');
-  const bxllinkedin = document.querySelector('.bxl-linkedin');
-  const bxlinstagram = document.querySelector('.bxl-instagram');
-  if (body.classList.contains('dark')) {
-    githubIcon.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
-    bxllinkedin.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
-    bxlinstagram.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
-  } else {
-    githubIcon.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
-    bxllinkedin.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
-    bxlinstagram.style.color = '#000'; // Alterando a cor do ícone para branco no Modo Dark
+  function toggleGitHubIconColor() {
+    const githubIcon = document.querySelector('.bxl-github');
+    const bxllinkedin = document.querySelector('.bxl-linkedin');
+    const bxlinstagram = document.querySelector('.bxl-instagram');
+    
+    if (body.classList.contains('dark')) {
+      githubIcon.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+      bxllinkedin.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+      bxlinstagram.style.color = '#fff'; // Alterando a cor do ícone para branco no Modo Dark
+    } else {
+      githubIcon.style.color = '#000'; // Alterando a cor do ícone para preto no Modo Light
+      bxllinkedin.style.color = '#000'; // Alterando a cor do ícone para preto no Modo Light
+      bxlinstagram.style.color = '#000'; // Alterando a cor do ícone para preto no Modo Light
+    }
   }
-}
 
-function toggleImageGradient() {
-  const imgWrapper = document.querySelector('.img-wrapper');
-  imgWrapper.classList.toggle('light'); // Alternar a classe light na .img-wrapper
-}
 
-function toggleButtonText() {
-  if (body.classList.contains('dark')) {
-    toggleButton.textContent = 'Modo Light'; // Alterando o texto do botão para "Modo Light" no Modo Dark
-  } else {
-    toggleButton.textContent = 'Modo Dark'; // Alterando o texto do botão para "Modo Dark" no Modo Light
+  function toggleImageGradient() {
+    const imgWrapper = document.querySelector('.img-wrapper');
+    imgWrapper.classList.toggle('light'); // Alternar a classe light na .img-wrapper
   }
-}
+
+  function toggleButtonText() {
+    if (body.classList.contains('dark')) {
+      toggleButton.textContent = 'Modo Light'; // Alterando o texto do botão para "Modo Light" no Modo Dark
+    } else {
+      toggleButton.textContent = 'Modo Dark'; // Alterando o texto do botão para "Modo Dark" no Modo Light
+    }
+  }
+
+  function toggleImageGradient() {
+    const imgWrapper = document.querySelector('.img-wrapper');
+    imgWrapper.classList.toggle('light'); // Alternar a classe light na .img-wrapper
+  }
+
